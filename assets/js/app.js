6ba0281f4b0f2d03edf51c1fdec35dd22e795686
Miriam Peralta <miiriampc@gmail.com>
2017-04-06 16:20:39 -0500

    desarrollo de estilos y transicion de menu

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',function(){
+  var menu=document.getElementsByClassName('menu');
+  var toggle=document.getElementById('toggle');
+  toggle.onclick=function(e){
+    e.preventDefault();
+    toggle.classList.toggle("open");
+  }
+});
